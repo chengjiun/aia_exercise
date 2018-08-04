397e27418e35472f332c012bbf339c09953dd147
at072142 <chengjiun.ma@gmail.com>
2018-08-04 10:12:44 +0000

    collect xml notebook

---
+++ helper.py
@@ -0,0 +1,29 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+
+def read_files2df(files):
+    df = pd.DataFrame()
+    for f in files:
+        record = read_xmlfile2dict(f)
+        sr = pd.Series(record, name=f)
+        df = df.append(sr)
+    return df
+    
+def read_xmlfile2dict(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    # print(xml_file, flush=True)
+    record={
+        'image_size_width': int(root.find('size').find('width').text),
+        'image_size_height': int(root.find('size').find('height').text),
+        'image_size_depth': int(root.find('size').find('depth').text),
+        'op_defect_name': root.find('object').find('name').text,
+        'subfolder': root.find('folder').text,
+        'pose': root.find('object').find('pose').text,
+        'xmin': root.find('object').find('bndbox').find('xmin').text,
+        'xmax': root.find('object').find('bndbox').find('xmax').text,
+        'ymin': root.find('object').find('bndbox').find('ymin').text,
+        'ymax': root.find('object').find('bndbox').find('xmax').text,
+    }
+    return record
